4fa2bc9fa430f899fc8147c32f9de296a7ef9884
Property404 <git@dagans.dev>
2025-03-16 15:37:56 -0500

    virtser: remove parity feature (doesn't work)

--- virtser/src/error.rs
+++ virtser/src/error.rs
@@ -1,17 +1,15 @@
 /// Result type for this crate
-pub type Result<T=()> = std::result::Result<T, Error>;
+pub type Result<T = ()> = std::result::Result<T, Error>;
 
 /// Error type for this crate
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error(transparent)]
-    Nix(nix::errno::Errno)
+    Nix(nix::errno::Errno),
 }
 
-impl From<nix::errno::Errno> for  Error {
+impl From<nix::errno::Errno> for Error {
     fn from(value: nix::errno::Errno) -> Self {
         Self::Nix(value)
     }
 }
-
-
--- virtser/src/lib.rs
+++ virtser/src/lib.rs
@@ -1,31 +1,39 @@
 #![allow(unused_mut, unused_imports, dead_code)]
-use std::{fs::File, io::{Read, Write}, os::fd::{AsRawFd, FromRawFd, IntoRawFd, OwnedFd, RawFd}, pin::{pin, Pin}, time::Duration};
-use nix::{errno::Errno, pty::{openpty, OpenptyResult}, sys::termios::{self, BaudRate, ControlFlags, LocalFlags, SetArg}};
+use nix::{
+    errno::Errno,
+    pty::{OpenptyResult, openpty},
+    sys::termios::{self, BaudRate, ControlFlags, LocalFlags, SetArg},
+};
+use std::{
+    fs::File,
+    io::{Read, Write},
+    os::fd::{AsRawFd, FromRawFd, IntoRawFd, OwnedFd, RawFd},
+    pin::{Pin, pin},
+    time::Duration,
+};
 mod error;
-pub use error::{Result, Error};
+pub use error::{Error, Result};
 
 pub struct VirtSer {
     master_file: File,
     slave_file: File,
-    slave_name: String
+    slave_name: String,
 }
 
 impl VirtSer {
     pub fn new() -> Result<Self> {
-        let OpenptyResult{master, slave} = openpty(None, None)?;
+        let OpenptyResult { master, slave } = openpty(None, None)?;
         set_nonblocking(master.as_raw_fd())?;
 
-
         let slave_name = get_slave_name(slave.as_raw_fd());
-        let slave_file = unsafe{File::from_raw_fd(slave.into_raw_fd())};
+        let slave_file = unsafe { File::from_raw_fd(slave.into_raw_fd()) };
         println!("{slave_name}: ");
 
         set_echo(&slave_file, false)?;
         set_baud_rate(&slave_file, BaudRate::B115200)?;
-        set_parity(&slave_file, Parity::None)?;
 
         let master_name = get_slave_name(master.as_raw_fd());
-        let mut master_file = unsafe{File::from_raw_fd(master.into_raw_fd())};
+        let mut master_file = unsafe { File::from_raw_fd(master.into_raw_fd()) };
         println!("{master_name}: ");
 
         master_file.write_all(b"Howdy y'all\n").unwrap();
@@ -33,7 +41,7 @@ impl VirtSer {
         Ok(Self {
             master_file,
             slave_file,
-            slave_name
+            slave_name,
         })
     }
 }
@@ -42,8 +50,12 @@ impl Read for VirtSer {
     fn read(&mut self, buf: &mut [u8]) -> std::io::Result<usize> {
         loop {
             match self.master_file.read(buf) {
-                Err(err) if err.kind() == std::io::ErrorKind::WouldBlock => {continue;},
-                other => {return other;}
+                Err(err) if err.kind() == std::io::ErrorKind::WouldBlock => {
+                    continue;
+                }
+                other => {
+                    return other;
+                }
             }
         }
     }
@@ -53,8 +65,13 @@ impl Write for VirtSer {
     fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
         loop {
             match self.master_file.write(buf) {
-                Err(err) if err.kind() == std::io::ErrorKind::WouldBlock => {std::thread::sleep(Duration::from_millis(100));continue;},
-                other => {return other;}
+                Err(err) if err.kind() == std::io::ErrorKind::WouldBlock => {
+                    std::thread::sleep(Duration::from_millis(100));
+                    continue;
+                }
+                other => {
+                    return other;
+                }
             }
         }
     }
@@ -93,36 +110,11 @@ fn set_baud_rate(file: &File, baud: BaudRate) -> Result {
     Ok(())
 }
 
-#[derive(Copy, Clone, PartialEq, Debug)]
-enum Parity {
-    Odd,
-    Even,
-    None
-}
-
-
-fn set_parity(file: &File, parity: Parity) -> Result {
-    let mut termio = termios::tcgetattr(file)?;
-    println!("Got termios");
-    match parity {
-        Parity::None => termio.control_flags &= !ControlFlags::PARENB,
-        Parity::Even => {
-            termio.control_flags |= ControlFlags::PARENB;
-            termio.control_flags &= !ControlFlags::PARODD;
-        }
-        Parity::Odd => termio.control_flags |= ControlFlags::PARENB | ControlFlags::PARODD,
-    }
-    println!("Sat termios");
-    termios::tcsetattr(file, SetArg::TCSANOW, &termio)?;
-    println!("Set termios");
-    Ok(())
-}
-
 fn set_echo(file: &File, echo: bool) -> Result {
-        let mut termios = termios::tcgetattr(&file)?;
-        if !echo {
-            termios.local_flags.remove(LocalFlags::ECHO);
-        }
-        termios::tcsetattr(&file, SetArg::TCSANOW, &termios)?;
-        Ok(())
+    let mut termios = termios::tcgetattr(&file)?;
+    if !echo {
+        termios.local_flags.remove(LocalFlags::ECHO);
+    }
+    termios::tcsetattr(&file, SetArg::TCSANOW, &termios)?;
+    Ok(())
 }
